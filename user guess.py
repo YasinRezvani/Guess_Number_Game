1dc5d5ec8a3732b480b810df97cddc4f394e9c27
YasinRezvani <yasinrezvani2025@gmail.com>
2021-08-10 09:46:59 +0430

    edit the inputs data

--- user guess.py
+++ user guess.py
@@ -10,15 +10,16 @@ def guess_user(x):
 
     while user_guess != random_num:
         table = BeautifulTable()
-        user_guess = int(input(f"Guess a number between 1 and {x}: "))
+        user_guess = int(input(f"\nGuess a number between 1 and {x}: "))
         count += 1
         if random_num > user_guess:
             print("--Guess again, Too low--")
         elif random_num < user_guess:
             print("--Guess again, Too high--")
-            
+
+    table.set_style(BeautifulTable.STYLE_BOX_ROUNDED)         
     table.rows.append([f"--You have guessed the number {random_num} correctly in {count} times"])
-    table.columns.header = ["You win"]
+    table.columns.header = ["You Win"]
     print(table)
 
 guess_user(10)
